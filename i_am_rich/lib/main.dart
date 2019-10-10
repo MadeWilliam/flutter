7901613d24f981c5cb8d62daa3e3375dad8371e2
MadeWilliam <pupul.mwm@gmail.com>
2019-10-10 12:54:26 +0700

    change text

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -6,7 +6,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Text('I Am Poor'),
+          title: Text('I Am Rich'),
           backgroundColor: Colors.blueGrey[900],
         ),
         body: Center(
