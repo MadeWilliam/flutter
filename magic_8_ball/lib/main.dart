f50da69a0c90641f5261160c7b259b186f2e5dd9
pupulstudy <pupul.study@gmail.com>
2019-10-18 12:57:37 +0700

    step 2

--- magic_8_ball/lib/main.dart
+++ magic_8_ball/lib/main.dart
@@ -2,6 +2,30 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text("Ask Me Anything"),
+      ),
+      body: HomeBody(),
+    );
+  }
+}
+
+class HomeBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      child: Text("data"),
+    );
+  }
+}
